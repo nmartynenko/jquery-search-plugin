f09bff3fd484d1e2d28f6007a0f11d2bcd3f4470
Nick Martynenko <n.martynenko@aimprosoft.com>
2014-04-23 15:04:03 +0300

    - added jQuery support in tests

--- test/testfile.js
+++ test/testfile.js
@@ -1,7 +1,43 @@
-var assert = require("assert");
+var assert = require("assert"),
+    jsdom = require("jsdom"),
+    jquery = require("jquery");
 
-describe("Application", function(){
-    it("should pass this test", function(){
-        assert.ok(true)
-    })
+//define JSDOM window
+var window = jsdom.jsdom().parentWindow;
+//expose global jQuery object
+jQuery = jquery(window);
+
+//Add libraries
+var fs = require('fs');
+var vm = require('vm');
+var includeInThisContext = function(path) {
+    var code = fs.readFileSync(path);
+    vm.runInThisContext(code, path);
+}.bind(this);
+
+includeInThisContext(__dirname + "/../lib/jquery.scrollTo.min.js");
+includeInThisContext(__dirname + "/../lib/jquery.textselect.min.js");
+includeInThisContext(__dirname + "/../jquery.search.min.js");
+
+/**
+ * Define all specs
+ */
+describe("Application's scope", function(){
+
+    it("should have jQuery object in the scope", function(){
+        assert.ok(!!jQuery === true);
+    });
+
+    it("should have jQuery.fn.scrollTo method in the scope", function(){
+        assert.ok(jQuery.isFunction(jQuery.fn.scrollTo));
+    });
+
+    it("should have jQuery.scrollTo method in the scope", function(){
+        assert.ok(jQuery.isFunction(jQuery.textSelect));
+    });
+
+    it("should have jQuery.search and jQuery.fn.search methods in the scope", function(){
+        assert.ok(jQuery.isFunction(jQuery.search));
+        assert.ok(jQuery.isFunction(jQuery.fn.search));
+    });
 });
